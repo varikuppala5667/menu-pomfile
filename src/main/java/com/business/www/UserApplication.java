a767e09a9f56371ac9dd3eb498bc5cfed80e4842
Mani137 <105615683+Mani137@users.noreply.github.com>
2024-11-26 20:10:52 +0530

    Update UserApplication.java

--- src/main/java/com/business/www/UserApplication.java
+++ src/main/java/com/business/www/UserApplication.java
@@ -1,15 +1,37 @@
+// package com.business.www;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class UserApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(UserApplication.class, args);
+// 		System.out.println("Hi your application running successfully");
+
+// 	}
+
+// }
+
 package com.business.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class UserApplication {
+public class UserApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
-		System.out.println("Hi your application running successfully");
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+        System.out.println("Hi, your application is running successfully");
+    }
 
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserApplication.class); // This is necessary for WAR packaging
+    }
 }
+
